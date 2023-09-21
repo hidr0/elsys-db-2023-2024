7a4956d0bb6a0cf5d73803dc40490036aec531b9
j7zd <76216704+j7zd@users.noreply.github.com>
2023-09-21 13:50:40 +0300

    we live we love we lie

---
+++ 21.09.2023/22_ognyan.sql
@@ -0,0 +1,22 @@
+drop database if exists db;
+-- (1)
+
+-- (2)
+create database db;
+
+-- (3)
+use db;
+
+-- (4), (5)
+create table students (
+	name varchar(255)
+);
+
+-- (6)
+insert into students(name) values ("Gosho");
+insert into students(name) values ("Pesho");
+insert into students(name) values ("Kircho");
+insert into students(name) values ("Christopher Balkanski");
+
+-- (7)
+
