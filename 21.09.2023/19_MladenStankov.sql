a399b5a6f0bd89278b4b3975e7ce984c13ff4ad1
MladenStankov <86181420+MladenStankov@users.noreply.github.com>
2023-09-26 11:23:21 +0300

    Update 19_MladenStankov.sql

--- 21.09.2023/19_MladenStankov.sql
+++ 21.09.2023/19_MladenStankov.sql
@@ -6,11 +6,13 @@ CREATE DATABASE gosho;
 USE gosho;
 -- Creating table
 CREATE TABLE  People (
-	PersonName varchar(255),
+    id int AUTO_INCREMENT PRIMARY KEY,
+    PersonName varchar(255),
     PersonAge int
 );
 -- Add rows
-INSERT INTO People (PersonName, PersonAge)
-VALUES ('Mladen Stankov', 20);
+INSERT INTO People (PersonName, PersonAge) VALUES ('Mladen Stankov', 20);
+INSERT INTO People (PersonName, PersonAge) VALUES ('Ivan Debel', 23);
+INSERT INTO People (PersonName, PersonAge) VALUES ('Kriza', 211);
 -- Checking table
 SELECT * FROM gosho.people
