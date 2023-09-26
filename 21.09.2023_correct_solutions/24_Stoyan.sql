17887e07a7a897fb6247db90501df4d1885b4b03
StoyanStoyanov05 <74011105+StoyanStoyanov05@users.noreply.github.com>
2023-09-26 02:20:12 -0700

    Add files via upload

--- 21.09.2023_correct_solutions/24_Stoyan.sql
+++ 21.09.2023_correct_solutions/24_Stoyan.sql
@@ -1,16 +1,15 @@
-DROP DATABASE IF EXISTS sClass;
-CREATE DATABASE sClass;
-USE sClass;
+DROP DATABASE IF EXISTS stoyan_class;
+CREATE DATABASE stoyan_class;
+USE stoyan_class;
 
-CREATE TABLE sClass (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE students_in_class(
+  id int AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(200),
-  age INT
+  age int
 );
 
-INSERT INTO sClass(name, age) VALUES ("Georgi", 26);
-INSERT INTO sClass(name, age) VALUES ("Boqn", 26);
-INSERT INTO sClass(name, age) VALUES ("Niki", 26);
-INSERT INTO sClass(name, age) VALUES ("Emo", 26);
-
-SELECT * FROM sClass;
+INSERT INTO students_in_class(name, age) VALUES("Georgi", 26);
+INSERT INTO students_in_class(name, age) VALUES("Boqn", 26);
+INSERT INTO students_in_class(name, age) VALUES("Niki", 26);
+INSERT INTO students_in_class(name, age) VALUES("Emo", 26);
+SELECT * FROM students_in_class;
