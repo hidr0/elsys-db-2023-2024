b967127a2b2f032e2bb60eb63b5b2bcee2731d36
Iva Marinova <ivamarinova06@gmail.com>
2023-09-26 11:36:31 +0300

    Add files via upload

---
+++ 21.09.2023_correct_solutions/11_Iva_Marinova.sql
@@ -0,0 +1,24 @@
+-- 1
+DROP DATABASE IF EXISTS Students;
+
+-- 2
+
+CREATE DATABASE Students;
+-- 3
+USE Students;
+
+-- 4 - 5
+CREATE TABLE AllStudents (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(40) NOT NULL,
+	age INT NOT NULL);
+    
+ -- 6
+ INSERT INTO AllStudents (name, age)
+	VALUES("Iva", 17);
+    
+ INSERT INTO AllStudents (name, age)
+	VALUES("Tomi", 16);
+    
+ -- 7
+ DESCRIBE AllStudents;
