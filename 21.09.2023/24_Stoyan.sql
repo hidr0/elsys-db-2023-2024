59c812ad6fdf29745369be3a75e875aeea7dd659
StoyanStoyanov05 <74011105+StoyanStoyanov05@users.noreply.github.com>
2023-09-26 02:19:05 -0700

    Add files via upload

--- 21.09.2023/24_Stoyan.sql
+++ 21.09.2023/24_Stoyan.sql
@@ -1,6 +1,15 @@
-Drop database if exists sHost;
-CREATE DATABASE sHost;
-CREATE TABLE Student(
-	StudentName varchar(255),
-    StudentId int 
+DROP DATABASE IF EXISTS stoyan_class;
+CREATE DATABASE stoyan_class;
+USE stoyan_class;
+
+CREATE TABLE students_in_class(
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(200),
+  age int
 );
+
+INSERT INTO students_in_class(name, age) VALUES("Georgi", 26);
+INSERT INTO students_in_class(name, age) VALUES("Boqn", 26);
+INSERT INTO students_in_class(name, age) VALUES("Niki", 26);
+INSERT INTO students_in_class(name, age) VALUES("Emo", 26);
+SELECT * FROM students_in_class;
