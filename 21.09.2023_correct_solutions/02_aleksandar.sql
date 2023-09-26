5b2354df6236b7bf8bf49eb8fb36e0eb70ff915f
Al1002 <71392792+Al1002@users.noreply.github.com>
2023-09-26 11:27:57 +0300

    Added select

---
+++ 21.09.2023_correct_solutions/02_aleksandar.sql
@@ -0,0 +1,16 @@
+-- (1)
+drop database if exists children;  
+-- (2)
+create database children;
+-- (3)
+use children;
+-- (4) (5) 
+create table students(
+	name tinytext,
+    age tinyint
+);
+-- (6) 
+insert into students values("Gosho", 127);
+insert into students values("Pesho", 12);
+-- (7) gosho is about 127 i think
+select * from students;
