2709f2675540cc1ba0b22b4a0be8d02fe0a7e39f
gosho535d <137002783+gosho535d@users.noreply.github.com>
2023-09-26 11:37:02 +0300

    Add files via upload

---
+++ 21.09.2023_correct_solutions/7_Georgi_Milev.sql
@@ -0,0 +1,23 @@
+-- 1;
+drop database if exists georgi;
+-- 2;
+Create  database georgi;
+-- 3;
+use dnevnik;
+-- 4,5;
+create table dnevnik (
+	years int,
+    Firstname varchar(255),
+    LastName varchar(255)
+    );
+
+-- 6;
+insert into dnevnik (ID,Firstname,Lastname)
+values (16,'Sasho','Petrov');
+insert into dnevnik (ID,Firstname,Lastname)
+values (14,'Gosho','Ivanov');
+insert into dnevnik (ID,Firstname,Lastname)
+values (15,'Pesho','Todorov');
+
+-- 7;
+select from *dnevnik;
