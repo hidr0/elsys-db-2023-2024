0c5cc6ec6be548a7e304e46b92623682960abe57
Pen4o1 <78597537+Pen4o1@users.noreply.github.com>
2023-10-10 08:33:25 +0100

    Add files via upload

---
+++ 10.10.2023/13_Ivan_Petrov.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF  EXISTS classwork;
+CREATE DATABASE classwork;
+USE classwork;
+
+CREATE TABLE Students(
+id int primary key auto_increment,
+name varchar(64),
+age int
+);
+
+INSERT INTO Students(name) VALUES("Ivan");
+INSERT INTO Students(name, age) VALUES("PEsho", 26);
+INSERT INTO Students(name, age) VALUES("Gosho", 21);
+INSERT INTO Students(name, age) VALUES("Pepi", 20);
+INSERT INTO Students(name, age) VALUES("Dani", 23);
+INSERT INTO Students(name, age) VALUES("Emo", 22);
+
+
+SELECT age, age, age FROM  Students;
+SELECT * FROM Students
+WHERE 20<age || 26>age;
+
+
+SELECT * FROM Students
+WHERE NOT (age=20)
+ORDER BY age ASC;
+
+-- chetni 
+SELECT * FROM Students
+WHERE NOT (age%2=0)
+ORDER BY age ASC;
+
+-- nechetni
+SELECT * FROM Students
+WHERE NOT (age%2=1)
+ORDER BY age ASC;
+
+-- dali se deli na 3 age
+SELECT * FROM Students
+WHERE NOT (age%3=0)
+ORDER BY age ASC;
