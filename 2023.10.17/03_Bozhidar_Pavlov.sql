b9dc47462a8b7409605dff6f4a51652307c7d5b2
Mihail Kirilov <hidr0.frbg@gmail.com>
2023-10-19 13:35:06 +0300

    Update 03_Bozhidar_Pavlov.sql

--- 2023.10.17/03_Bozhidar_Pavlov.sql
+++ 2023.10.17/03_Bozhidar_Pavlov.sql
@@ -62,3 +62,42 @@ where StudentID = 1;
 update Students
 set Email = 'jane.doe@example.com'
 where StudentID = 2;
+
+
+-- **Grading and Feedback**
+
+-- Let's assess the provided solution step by step:
+
+-- 1. **Creation of Database:**
+--    - Drops the `SchoolDB` database if it exists, creates it anew, and uses it for subsequent operations. **✔️**
+
+-- 2. **Creation of Tables:**
+--    - `Students` and `Courses` tables are created with appropriate fields. The use of `serial` indicates a PostgreSQL database, as this datatype is specific to PostgreSQL and is used for auto-incrementing integer columns. **✔️**
+
+-- 3. **Data Insertion:**
+--    - Five student records and three course records are correctly inserted into their respective tables. **✔️**
+
+-- 4. **Data Retrieval:**
+--    - Fetches students living at "123 Elm Street" correctly. **✔️**
+--    - Retrieves students named "John" born on or after 2001 correctly. **✔️**
+--    - Accurately fetches courses named "Mathematics" or "History". **✔️**
+--    - Correctly fetches students with a `NULL` address. **✔️**
+
+-- 5. **Data Update:**
+--    - The address of the student with `StudentID = 3` is updated as specified. **✔️**
+--    - The description of the "Mathematics" course is updated, but it's redundant since the description being set is the same as the one inserted earlier. While not wrong, this may be unnecessary unless there was an expectation of a different description initially. **✔️**
+
+-- 6. **Data Deletion:**
+--    - The student named "Jane Doe" is correctly deleted from the database. **✔️**
+
+-- 7. **Bonus Task:**
+--    - The `ALTER TABLE` operation adds an `Email` column to the `Students` table. **✔️**
+--    - The email addresses for students with `StudentID = 1` and `StudentID = 2` are updated correctly. However, note that `StudentID = 2` corresponds to 'Jane Doe' who was deleted in the previous step, so this update operation would not affect any rows. **✔️**
+
+-- **Summary:** 
+
+-- The solution is comprehensive, clear, and meets the task requirements. There's a minor oversight in updating the email for a deleted student.
+
+-- **Grade:** 99/100 (One point deduction due to the redundant update for a deleted student)
+
+-- Well done!
