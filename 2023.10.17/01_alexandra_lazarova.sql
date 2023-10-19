9b3bc4a25a3e843a01adff0d71e4b6f7c35db6f9
Mihail Kirilov <hidr0.frbg@gmail.com>
2023-10-19 13:28:25 +0300

    Update 01_alexandra_lazarova.sql

--- 2023.10.17/01_alexandra_lazarova.sql
+++ 2023.10.17/01_alexandra_lazarova.sql
@@ -54,3 +54,34 @@ ALTER TABLE Students ADD COLUMN Email VARCHAR(100);
 UPDATE Students
 SET Email = 'ali@example.com'
 WHERE StudentID IN (1, 2);
+
+-- Grading and Feedback
+
+-- Overall, the solution you've provided follows the specifications provided in the assignment tasks quite closely. Here's the feedback:
+
+-- Creation of Database and Tables:
+-- The script first checks if the database SchoolDB exists and drops it if it does. Then, it proceeds to create the SchoolDB database and sets it as the default for subsequent operations. This approach ensures a fresh start every time the script is run. ✔️
+-- The Students and Courses tables are created as specified, with appropriate data types and constraints. ✔️
+-- Insertion of Data:
+-- Data insertion for both the Students and Courses tables is correct. Five students and three courses are inserted as described in the tasks. ✔️
+-- Data Retrieval:
+-- The query to fetch students living at "123 Elm Street" is accurate. ✔️
+-- The query to retrieve students named "Zlati" born after 2000 is correct. ✔️
+-- The query for fetching courses named "Mathematics" or "History" is accurate. ✔️
+-- The query to fetch students with a NULL address is correct. ✔️
+-- Data Update:
+-- The update for the address of the student with StudentID = 3 is correct. ✔️
+-- The update for the course description of "Mathematics" matches the task's requirement. ✔️
+-- Data Deletion:
+-- The student named "Zlati Antov" is correctly deleted. ✔️
+-- There's a minor issue in the deletion task. The script attempts to delete a course named "Physical Education", but this course wasn't added in the data insertion step. Therefore, this operation would have no effect. ❌
+-- Bonus Task:
+-- The ALTER TABLE operation adds an Email column to the Students table as specified. ✔️
+-- The update operation to set email addresses for students with StudentID = 1 and StudentID = 2 is accurate. ✔️
+-- Summary:
+
+-- The solution is very well-structured, and the SQL commands are appropriately written. Most tasks are correctly implemented. One minor error was noticed in the deletion task, where an attempt to delete a non-existent course was made. Make sure to double-check the requirements and the implemented solution to ensure every task is addressed accurately.
+
+-- Grade: 98/100 (Deduction due to the deletion error)
+
+-- Great job!
