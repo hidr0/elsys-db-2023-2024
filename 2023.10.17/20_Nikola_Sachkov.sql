178a4fae13a702d53bad700f16873046569c666a
Mihail Kirilov <hidr0.frbg@gmail.com>
2023-10-26 11:47:39 +0300

    Update 20_Nikola_Sachkov.sql

--- 2023.10.17/20_Nikola_Sachkov.sql
+++ 2023.10.17/20_Nikola_Sachkov.sql
@@ -46,3 +46,40 @@ ALTER TABLE Students ADD Email VARCHAR(100);
 UPDATE Students
 SET Email = 'john.doe@example.com'
 WHERE StudentID IN (1, 2);
+
+
+-- The SQL script you've provided seems to be well-structured, but let's go through each part and verify its accuracy:
+
+-- 1. **Database Creation**:
+--     - The existing `SchoolDB` database is dropped if it exists.
+--     - A new database `SchoolDB` is created and set as the active database. **✔️**
+
+-- 2. **Table Creation**:
+--     - The `Students` and `Courses` tables are created with appropriate columns. **✔️**
+
+-- 3. **Data Insertion**:
+--     - Five students are inserted into the `Students` table. **✔️**
+--     - Three courses are inserted into the `Courses` table. **✔️**
+
+-- 4. **Data Retrieval**:
+--     - It fetches students living at '123 Elm Street'. **✔️**
+--     - Retrieves students named 'John' with DOB after January 1, 2000. **✔️**
+--     - Retrieves courses named 'Mathematics' or 'History'. **✔️**
+--     - Retrieves students without an address. **✔️**
+
+-- 5. **Data Update**:
+--     - The address of the student with `StudentID = 3` is updated. **✔️**
+--     - The description of the 'Mathematics' course is updated. **✔️**
+
+-- 6. **Data Deletion**:
+--     - The student named 'Jane Doe' is deleted. **✔️**
+--     - There's an attempt to delete the 'Physical Education' course, but it was not inserted in the first place, so this DELETE command will not remove any records. **❌** This isn't a script-breaking error, but it does indicate a discrepancy between the data you intended to create and the data you intended to delete.
+
+-- 7. **Table Modification**:
+--     - An 'Email' column is added to the `Students` table. **✔️**
+--     - Emails are updated for students with specific StudentIDs. **✔️**
+
+-- **Summary**: 
+-- The script appears to be almost entirely correct, with only one minor discrepancy. The attempt to delete a course that was never added might indicate that there was a change in plans or a minor oversight during the script's creation.
+
+-- **Grade**: 98/100 due to the mentioned discrepancy.
