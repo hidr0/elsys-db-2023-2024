62ecc342c549c0b991140d72461fa601ba916406
Mihail Kirilov <hidr0.frbg@gmail.com>
2023-10-26 11:11:29 +0300

    Update 09_Denis_Mirchev.sql

--- 2023.10.17/09_Denis_Mirchev.sql
+++ 2023.10.17/09_Denis_Mirchev.sql
@@ -63,3 +63,42 @@ WHERE StudentID = 1;
 UPDATE Students
 SET Email = 'jane.doe@example.com'
 WHERE StudentID = 2;
+
+-- Let's review and grade the provided SQL code based on the assignment:
+
+-- 1. **Database Creation**:
+--    - The code correctly creates the `SchoolDB` database, deletes it first if it exists, and then sets it as the active database. **✔️**
+
+-- 2. **Table Creation**:
+--    - The `Students` table is correctly defined with the necessary columns. **✔️**
+--    - The `Courses` table is also correctly defined. **✔️**
+
+-- 3. **Data Insertion**:
+--    - Five student records are inserted into the `Students` table. All given values are correct and match the assignment. **✔️**
+--    - Three courses are inserted into the `Courses` table. All given values are correct and match the assignment. **✔️**
+
+-- 4. **Data Retrieval**:
+--    - The query correctly retrieves students residing at '123 Elm Street'. **✔️**
+--    - It correctly retrieves students named 'John' born on or after the year 2001. **✔️**
+--    - The query correctly retrieves the courses 'Mathematics' and 'History'. **✔️**
+--    - It correctly retrieves students without an address. **✔️**
+
+-- 5. **Data Update**:
+--    - The address of the student with `StudentID = 3` is correctly updated. **✔️**
+--    - The description of the 'Mathematics' course is correctly updated. **✔️**
+
+-- 6. **Data Deletion**:
+--    - The student named 'Jane Doe' is correctly deleted from the database. **✔️**
+--    - The `DELETE` command for the 'Physical Education' course will not delete any record since that course was not initially inserted. It doesn't raise an error, but it's an unnecessary command. **❌**
+
+-- 7. **Bonus Task**:
+--    - The `Email` column is added to the `Students` table without issues. **✔️**
+--    - The emails for students with `StudentID = 1` and `StudentID = 2` are correctly updated. **✔️**
+
+-- **Summary**:
+-- The script is very well constructed and almost completely matches the assignment. Only a minor inconsistency in the deletion step prevents it from being perfect.
+
+-- **Grade**: 98/100
+
+-- Areas of Improvement:
+-- - Align data manipulation commands (like DELETE) with the previously inserted data to avoid redundancy or unnecessary commands.
