2968e3a5acd22ed3290fb17e1c8c25c4a709de8d
Mihail Kirilov <hidr0.frbg@gmail.com>
2023-10-26 11:23:48 +0300

    Update 14_ivan_matev.sql

--- 2023.10.17/14_ivan_matev.sql
+++ 2023.10.17/14_ivan_matev.sql
@@ -63,3 +63,40 @@ DELETE FROM Students
 WHERE FirstName = 'Jane' AND LastName = 'Doe';
 
 DELETE FROM Courses WHERE CourseName = 'Physical Education';
+
+-- Let's review and grade the provided SQL script:
+
+-- 1. **Database Creation**:
+--    - The script correctly creates the `SchoolDB` database, deletes it first if it exists, and then sets it as the active database. **✔️**
+
+-- 2. **Table Creation**:
+--    - The `Students` table is correctly defined with necessary columns: StudentId, FirstName, LastName, Address, and DOB. **✔️**
+--    - The `Courses` table is correctly defined with necessary columns: CourseID, CourseName, and CourseDescription. **✔️**
+
+-- 3. **Data Insertion**:
+--    - Five student records are inserted into the `Students` table. All values are correct and match the assignment. **✔️**
+--    - Three courses are inserted into the `Courses` table. All given values are correct and match the assignment. **✔️**
+
+-- 4. **Data Retrieval**:
+--    - The query correctly retrieves students residing at '123 Elm Street'. **✔️**
+--    - It correctly retrieves students named 'John' born after the year 2000. **✔️**
+--    - It correctly retrieves records from the `Courses` table with course names 'Mathematics' or 'History'. **✔️**
+--    - It correctly retrieves students without an address. In this particular dataset, no student is without an address, so it will return an empty set, but the logic is correct. **✔️**
+
+-- 5. **Data Update**:
+--    - The address of the student with `StudentID = 3` is correctly updated. **✔️**
+--    - The description of the 'Mathematics' course is correctly updated. **✔️**
+
+-- 6. **Data Deletion**:
+--    - It correctly deletes the student named 'Jane Doe'. **✔️**
+--    - The delete statement correctly deletes the course named 'Physical Education'. **✔️**
+
+-- **Note**: The SQL script includes some unconventional (and informal) descriptions in values (e.g., "Cifri and shit" and "20 licevi"), which are not typical in a professional or academic environment. When preparing SQL scripts or data for real-world situations or presentations, it's essential to use clear and appropriate descriptions and comments.
+
+-- **Summary**:
+-- The script is well-structured and correctly executes all the specified tasks.
+
+-- **Grade**: 100/100
+
+-- Recommendation:
+-- - Always use appropriate language and descriptions, especially in a professional or academic setting. The choice of words in the course descriptions is unprofessional.
