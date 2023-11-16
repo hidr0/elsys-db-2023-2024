daf1f8d88fb3c64a12a4e768a46b77ff12833ef2
BvHand487 <84194378+BvHand487@users.noreply.github.com>
2023-11-16 13:47:10 +0200

    Update 04_boris.sql

--- 2023.11.16/04_boris.sql
+++ 2023.11.16/04_boris.sql
@@ -4,7 +4,7 @@ CREATE DATABASE ComputerStore;
 USE ComputerStore;
 
 -- Products: Stores information about computers for sale
-CREATE TABLE Products (
+CREATE TABLE IF NOT EXISTS Products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     description VARCHAR(255),
@@ -18,11 +18,12 @@ INSERT INTO Products(name, description, price, stock_quantity, category) VALUES
 	("Macbook Pro 16GB", "Very fast laptop", 3600, 10, "laptop"),
 	("Macbook Pro 64GB", "Very fast laptop", 800, 1, "laptop"),
 	("Macbook Pro 96GB", "Very fast laptop", 7200, 0, "laptop"),
-	("IPad Air II", "The tablet of tablets", 1000, 1, "tablet");
+	("IPad Air II", "The tablet of tablets", 1000, 1, "tablet"),
+	("Gaming Computer", "Best gaming computer ever", 3000, 1, "desktop");
 
 
 -- Stores information about customers
-CREATE TABLE Customers (
+CREATE TABLE IF NOT EXISTS Customers (
     id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100),
     email VARCHAR(100),
@@ -30,11 +31,12 @@ CREATE TABLE Customers (
 );
 
 INSERT INTO Customers(full_name, email, address) VALUES
-	("Bay Ganyo", "bayganyo@abv.bg", "Chicago");
+	("Bay Ganyo", "bayganyo@abv.bg", "Chicago"),
+	("Jacob", "123@example.com", "Burgas");
 
 
 -- Orders: Stores information about customer orders
-CREATE TABLE Orders (
+CREATE TABLE IF NOT EXISTS Orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     order_date DATETIME,
     total_amount INT,
@@ -56,14 +58,6 @@ SELECT name FROM Products WHERE stock_quantity <= 0;
 -- List the details of orders placed in the last 30 days.
 SELECT * FROM Orders WHERE order_date >= CURDATE() - INTERVAL 30 DAY;
 
--- 2. UPDATE Query:
--- Update the stock quantity of a specific product after a new shipment arrives.
-UPDATE Products SET stock_quantity = stock_quantity + 20 WHERE name LIKE "Macbook Pro 64GB";
-
--- 3. DELETE Query:
--- Delete all products that have been discontinued and are no longer in stock.
-DELETE FROM Products WHERE stock_quantity <= 0;
-
 -- 17. Date and Time Functions:
 -- List all orders placed on weekends.
 SELECT * FROM Orders WHERE WEEKDAY(order_date) IN (5, 6);
@@ -84,3 +78,22 @@ SELECT MAX(price), category FROM Products GROUP BY category;
 
 -- Which person which order?
 SELECT * FROM Orders LEFT JOIN Customers ON Orders.customer_id = Customers.id;
+
+
+-- 2. UPDATE Query:
+-- Update the stock quantity of a specific product after a new shipment arrives.
+UPDATE Products SET stock_quantity = stock_quantity + 20 WHERE name LIKE "Macbook Pro 64GB";
+
+-- 3. DELETE Query:
+-- Delete all products that have been discontinued and are no longer in stock.
+DELETE FROM Products WHERE stock_quantity <= 0;
+
+-- 7. Count the total number of orders placed each month.
+SELECT COUNT(*), MONTHNAME(order_date) FROM Orders GROUP BY MONTHNAME(order_date);
+
+-- 14. Using LIKE for Pattern Matching:
+-- Select product names that contain the word 'Gaming'.
+SELECT name FROM Products WHERE name LIKE "%Gaming%";
+
+-- Find customer names that start with the letter 'J'.
+SELECT full_name FROM Customers WHERE full_name LIKE "J%";
