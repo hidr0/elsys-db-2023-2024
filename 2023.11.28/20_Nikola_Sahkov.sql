b20944f9b5cf00a9d692cf6861928570457e18f8
Nikola Sachkov <79465110+nikolasachkov@users.noreply.github.com>
2023-11-30 11:22:58 +0200

    Add files via upload

---
+++ 2023.11.28/20_Nikola_Sahkov.sql
@@ -0,0 +1,68 @@
+DROP DATABASE IF EXISTS zoo;
+CREATE DATABASE zoo;
+USE zoo;
+
+CREATE TABLE Person(
+	id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(20) unique
+);
+
+CREATE TABLE Dog(
+	id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(20) unique,
+    breed ENUM("Golden", "Frenchie", "Labrador", "Husky"),
+    person_id int not null,
+    FOREIGN KEY(person_id) REFERENCES Person(id)
+);
+
+INSERT INTO Person(name) VALUES("Misho");
+INSERT INTO Person(name) VALUES("Nakso");
+INSERT INTO Person(name) VALUES("Dimitar");
+INSERT INTO Person(name) VALUES("Georgi");
+
+
+INSERT INTO DOG(name, breed, person_id) VALUES("Chocho", "Labrador", 1);
+INSERT INTO DOG(name, breed, person_id) VALUES("Pow pow", "Golden", 1);
+INSERT INTO DOG(name, breed, person_id) VALUES("GG", "Golden", 1);
+INSERT INTO DOG(name, breed, person_id) VALUES("Shaq", "Husky", 3);
+INSERT INTO DOG(name, breed, person_id) VALUES("Bau bau", "Frenchie", 2);
+
+
+
+Select * from Person;
+Select * from Dog;
+
+
+
+SELECT * FROM Dog
+LEFT JOIN Person
+ON Dog.person_id = Person.id;
+
+SELECT * FROM Person
+RIGHT JOIN Dog
+ON Dog.person_id = Person.id;
+
+
+SELECT * FROM Person
+LEFT JOIN Dog
+ON Dog.person_id = Person.id;
+
+-- All people that do not have dogs.
+SELECT Person.name FROM Person
+LEFT JOIN Dog
+ON Dog.person_id = Person.id
+WHERE Dog.id IS NULL;
+
+-- How many people have a dog
+SELECT COUNT(DISTINCT Person.name) FROM Dog
+LEFT JOIN Person
+ON Dog.person_id = Person.id;
+
+-- How many dogs has each person
+SELECT Person.name, Count(Dog.id) FROM Person
+LEFT JOIN DOG ON Person.id = Dog.person_id
+GROUP BY Person.name;
+
+SELECT Person.name, Dog.breed, Count(Dog.id) FROM Person
+LEFT JOIN DOG ON Person.id = Dog.person_id
+GROUP BY Person.name, Dog.breed;
