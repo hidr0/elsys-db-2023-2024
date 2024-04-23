2a140d09568a7d7708f2f8a0eccec80e65167544
StoyanStoyanov05 <74011105+StoyanStoyanov05@users.noreply.github.com>
2024-04-23 05:55:26 -0700

    Add files via upload

--- 2024.04.16/24_Stoyan.sql
+++ 2024.04.16/24_Stoyan.sql
@@ -5,7 +5,7 @@ USE PhoneCalls;
 CREATE TABLE calls (
     id INT AUTO_INCREMENT PRIMARY KEY,
     number VARCHAR(20),
-    uuid VARCHAR(255) NOT NULL,
+    uuid VARCHAR(36) NOT NULL,
     country VARCHAR(50),
     caller_number INT
 );
@@ -13,15 +13,13 @@ CREATE TABLE calls (
 CREATE TABLE ping (
     id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255),
-    response JSON DEFAULT '{"name": "Pesho", "age": "12", "gender": "male"}', 
+    response JSON,
     call_id INT,
     FOREIGN KEY (call_id) REFERENCES calls(id)
 );
 
 CREATE TABLE tag_value (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    call_id INT, 
     value VARCHAR(255)
 );
 
@@ -33,10 +31,26 @@ CREATE TABLE tag (
     FOREIGN KEY (tag_value_id) REFERENCES tag_value(id)
 );
 
-CREATE TABLE tag_value_call (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE call_tag (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     call_id INT,
-    tag_value_id INT,
+    tag_id INT,
     FOREIGN KEY (call_id) REFERENCES calls(id),
-    FOREIGN KEY (tag_value_id) REFERENCES tag_value(id)
+    FOREIGN KEY (tag_id) REFERENCES tag(id)
 );
+
+INSERT INTO calls (number, uuid, country, caller_number) VALUES 
+    ('238710854', REPLACE(UUID(), '-', ''), 'BG', 285018295),
+    ('357381924', REPLACE(UUID(), '-', ''), 'US', 285839523);
+
+INSERT INTO ping (url, response, call_id)
+VALUES 
+    ('https://example.com/', '{"name": "Pesho", "age": "12", "gender": "male"}', 1),
+    ('https://example.com/', '{"name": "Pesho", "age": "18", "gender": "male"}', 2);
+     
+SELECT calls.*
+FROM calls
+INNER JOIN ping ON calls.id = ping.call_id
+WHERE JSON_EXTRACT(ping.response, '$.name') = 'Pesho';
+
+ALTER TABLE tag MODIFY created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
